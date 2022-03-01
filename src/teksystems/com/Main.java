356cdecf4fbd95d926334fa2dd0d7e338f1a3b2d
EsmondKim <ekelectronicmail@gmail.com>
2022-02-28 22:28:09 -0600

    Through createArray123().

---
+++ src/teksystems/com/Main.java
@@ -0,0 +1,16 @@
+package teksystems.com;
+
+public class Main {
+    public static void main(String[] args) {
+        createArray123();
+    } //main
+
+    public static void createArray123() {
+        int[] arr123 = {1, 2, 3};
+        for (int i = 0; i < arr123.length; i++)
+        System.out.println(arr123[i]);
+    }//createArray123()
+
+    
+
+}//Main
